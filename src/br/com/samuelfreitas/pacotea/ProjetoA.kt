8e252686921600f42302cb418f38e0fe2fbf92c0
freitassp <priettosamuel@gmail.com>
2021-06-27 10:10:05 -0300

    Initial commit

---
+++ src/br/com/samuelfreitas/pacotea/ProjetoA.kt
@@ -0,0 +1,2 @@
+package br.com.samuelfreitas.pacotea
+fun name() = "Pacote A"
---
+++ src/br/com/samuelfreitas/pacoteb/PacoteB.kt
@@ -0,0 +1,9 @@
+package br.com.samuelfreitas.pacoteb
+import br.com.samuelfreitas.pacotea.name
+
+fun main(){
+
+    println(name())
+
+
+}
